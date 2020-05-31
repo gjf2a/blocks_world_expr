fc8170dff6decc8a842c95dc4e4dc78db26e23bc
gjf2a <ferrer@hendrix.edu>
2020-05-30 19:23:45 -0500

    Updated to reflect changes to anyhop.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, io, fs};
 use blocks_world::pddl_parser::make_block_problem_from;
 use anyhop::BacktrackPreference::*;
 use anyhop::BacktrackStrategy::*;
-use anyhop::AnytimePlanner;
+use anyhop::AnytimePlannerBuilder;
 use std::fs::File;
 use std::io::Write;
 
@@ -43,7 +43,12 @@ fn assess_file(file: &str, results: &mut String, limit_ms: Option<u128>) -> io::
     let (start, goal) = make_block_problem_from(file)?;
     for strategy in vec![Alternate(LeastRecent), Steady(LeastRecent), Steady(MostRecent)] {
         for apply_cutoff in vec![true, false] {
-            let outcome = AnytimePlanner::plan(&start, &goal, limit_ms, strategy, &|p| p.len(), 1, apply_cutoff);
+            let outcome = AnytimePlannerBuilder::state_goal(&start, &goal)
+                .apply_cutoff(apply_cutoff)
+                .strategy(strategy)
+                .possible_time_limit_ms(limit_ms)
+                .verbose(1)
+                .construct();
             let label = format!("o_{}_{:?}_{}", desuffix(file), strategy, if apply_cutoff {"cutoff"} else {"no_cutoff"}).replace(")", "_").replace("(", "_");
             let row = outcome.summary_csv_row(label.as_str());
             print!("{}", row);
