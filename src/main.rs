59b4269ece5b0524a3cb41659e7ac7f2eecc4ac3
gjf2a <ferrer@hendrix.edu>
2020-07-21 12:14:55 -0500

    Added simple_logger.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+extern crate log;
+extern crate simple_logger;
+
 use std::io;
 use blocks_world::pddl_parser::make_block_problem_from;
 use anyhop::{process_expr_cmd_line, CmdArgs};
 
 fn main() -> io::Result<()> {
+    simple_logger::init().unwrap();
     let args = CmdArgs::new()?;
     process_expr_cmd_line(&make_block_problem_from, &args)
 }
